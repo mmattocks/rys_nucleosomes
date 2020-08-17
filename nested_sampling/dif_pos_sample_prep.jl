d318ff9c9ccfbcf9c3f6d712b39fce16db0a1e09
Michael Mattocks <mike.mattocks@utoronto.ca>
2020-08-17 13:46:48 -0400

    prior test ops

--- nested_sampling/dif_pos_sample_prep.jl
+++ nested_sampling/dif_pos_sample_prep.jl
@@ -1,4 +1,4 @@
-using BioSequences, Distributed, DataFrames, nucpos, BGHMM, CSV, Serialization
+using BioSequences, Distributed, DataFrames, nucpos, BioBackgroundModels, CSV, Serialization
 
 sib_pos = "/bench/PhD/danpos_results/pooled/sib.Fnor.smooth.positions.xls"
 rys_pos = "/bench/PhD/danpos_results/pooled/rys.Fnor.smooth.positions.xls"
@@ -7,19 +7,19 @@ danio_genome_path = "/bench/PhD/seq/GRCz11/GCA_000002035.4_GRCz11_genomic.fna"
 danio_gen_index_path = "/bench/PhD/seq/GRCz11/GCA_000002035.4_GRCz11_genomic.fna.fai"
 danio_gff_path = "/bench/PhD/seq/GRCz11/Danio_rerio.GRCz11.94.gff3"
 
-selected_hmm_output = "/bench/PhD/NGS_binaries/BGHMM/selected_BGHMMs"
+refined_folders_path = "/bench/PhD/NGS_binaries/BBM/refined_folders"
 
-sib_df_binary = "/bench/PhD/NGS_binaries/nnlearn/sib_diff_positions"
-rys_df_binary = "/bench/PhD/NGS_binaries/nnlearn/rys_diff_positions"
-combined_df_binary = "/bench/PhD/NGS_binaries/nnlearn/combined_diff_positions"
+sib_df_binary = "/bench/PhD/NGS_binaries/BMI/sib_diff_positions"
+rys_df_binary = "/bench/PhD/NGS_binaries/BMI/rys_diff_positions"
+combined_df_binary = "/bench/PhD/NGS_binaries/BMI/combined_diff_positions"
 
-sib_code_binary = "/bench/PhD/NGS_binaries/nnlearn/sib_diff_codes"
-rys_code_binary = "/bench/PhD/NGS_binaries/nnlearn/rys_diff_codes"
-combined_code_binary = "/bench/PhD/NGS_binaries/nnlearn/combined_diff_codes"
+sib_code_binary = "/bench/PhD/NGS_binaries/BMI/sib_diff_codes"
+rys_code_binary = "/bench/PhD/NGS_binaries/BMI/rys_diff_codes"
+combined_code_binary = "/bench/PhD/NGS_binaries/BMI/combined_diff_codes"
 
-sib_diff_bg = "/bench/PhD/NGS_binaries/nnlearn/sib_diff_bg"
-rys_diff_bg = "/bench/PhD/NGS_binaries/nnlearn/rys_diff_bg"
-combined_diff_bg = "/bench/PhD/NGS_binaries/nnlearn/combined_diff_bg"
+sib_diff_bg = "/bench/PhD/NGS_binaries/BMI/sib_diff_bg"
+rys_diff_bg = "/bench/PhD/NGS_binaries/BMI/rys_diff_bg"
+combined_diff_bg = "/bench/PhD/NGS_binaries/BMI/combined_diff_bg"
 
 sib_fa = "/bench/PhD/thicweed_results/sib_nuc_position_sequences.fa"
 sib_arch = "/bench/PhD/thicweed_results/sib_nuc_position_sequences.fa_archs.txt"
@@ -32,15 +32,8 @@ sib_df=CSV.read(sib_pos)
 rys_df=CSV.read(rys_pos)
 
 @info "Mapping..."
-Threads.@threads for t in 1:Threads.nthreads()
-    if Threads.threadid()==1
-        nucpos.map_positions!(sib_df, rys_df)
-    elseif Threads.threadid()==2
-        nucpos.map_positions!(rys_df, sib_df)
-    else
-        break
-    end
-end
+nucpos.map_positions!(sib_df, rys_df)
+nucpos.map_positions!(rys_df, sib_df)
 
 @info "Making differential position dataframes..."
 sib_diff_df = deepcopy(sib_df[findall(iszero,sib_df.mapped_pos),:])
@@ -56,19 +49,12 @@ deleterows!(sib_diff_df, [hasambiguity(seq) for seq in sib_diff_df.seq])
 deleterows!(rys_diff_df, [hasambiguity(seq) for seq in rys_diff_df.seq])
 
 @info "Masking positions by genome partition and strand..."
-BGHMM.add_partition_masks!(sib_diff_df, danio_gff_path, 500, (:chr,:seq,:start))
-BGHMM.add_partition_masks!(rys_diff_df, danio_gff_path, 500, (:chr,:seq,:start))
+BioBackgroundModels.add_partition_masks!(sib_diff_df, danio_gff_path, 500, (:chr,:seq,:start))
+BioBackgroundModels.add_partition_masks!(rys_diff_df, danio_gff_path, 500, (:chr,:seq,:start))
 
 @info "Obtaining cluster data..."
-Threads.@threads for t in 1:Threads.nthreads()
-    if Threads.threadid()==1
-        nucpos.get_cluster!(sib_diff_df, sib_fa, sib_arch)
-    elseif Threads.threadid()==2
-        nucpos.get_cluster!(rys_diff_df, rys_fa, rys_arch)
-    else
-        break
-    end
-end
+nucpos.get_cluster!(sib_diff_df, sib_fa, sib_arch)
+nucpos.get_cluster!(rys_diff_df, rys_fa, rys_arch)
 
 @info "Serializing dataframes..."
 combined_diff_df = vcat(sib_diff_df, rys_diff_df)
@@ -87,13 +73,17 @@ serialize(sib_code_binary, Matrix(transpose(sib_codes)))
 serialize(rys_code_binary, Matrix(transpose(rys_codes)))
 serialize(combined_code_binary, Matrix(transpose(combined_codes)))
 
-@info "Setting up for BGHMM likelihood calculations..."
-BGHMM_dict = deserialize(selected_hmm_output)
+@info "Setting up for BBM likelihood calculations..."
+BHMM_dict = Dict{String,BHMM}()
+refined_folders=deserialize(refined_folders_path)
+for (part, folder) in refined_folders
+    BHMM_dict[part]=folder.partition_report.best_model[2]
+end
 
 @info "Performing calculations..."
-sib_lh_matrix = BGHMM.BGHMM_likelihood_calc(sib_diff_df, BGHMM_dict, symbol=:seq)
-rys_lh_matrix = BGHMM.BGHMM_likelihood_calc(rys_diff_df, BGHMM_dict, symbol=:seq)
-combined_lh_matrix = BGHMM.BGHMM_likelihood_calc(rys_diff_df, BGHMM_dict, symbol=:seq)
+sib_lh_matrix = BGHMM_likelihood_calc(sib_diff_df, BHMM_dict, symbol=:seq)
+rys_lh_matrix = BGHMM_likelihood_calc(rys_diff_df, BHMM_dict, symbol=:seq)
+combined_lh_matrix = BGHMM_likelihood_calc(rys_diff_df, BHMM_dict, symbol=:seq)
 
 @info "Serializing background matrices..."
 serialize(sib_diff_bg, sib_lh_matrix)
---
+++ nested_sampling/prior_test.jl
@@ -0,0 +1,111 @@
+@info "Setting up for job..."
+#JOB FILEPATHS
+sib_wms_path = "/bench/PhD/NGS_binaries/BMI/sib_nuc_position_sequences.fa_wms.tr"
+rys_wms_path = "/bench/PhD/NGS_binaries/BMI/rys_nuc_position_sequences.fa_wms.tr"
+
+sib_df_binary = "/bench/PhD/NGS_binaries/BMI/sib_diff_positions"
+rys_df_binary = "/bench/PhD/NGS_binaries/BMI/rys_diff_positions"
+combined_df_binary = "/bench/PhD/NGS_binaries/BMI/combined_diff_positions"
+
+sib_code_binary = "/bench/PhD/NGS_binaries/BMI/sib_diff_codes"
+rys_code_binary = "/bench/PhD/NGS_binaries/BMI/rys_diff_codes"
+combined_code_binary = "/bench/PhD/NGS_binaries/BMI/combined_diff_codes"
+
+sib_diff_bg = "/bench/PhD/NGS_binaries/BMI/sib_diff_bg"
+rys_diff_bg = "/bench/PhD/NGS_binaries/BMI/rys_diff_bg"
+combined_diff_bg = "/bench/PhD/NGS_binaries/BMI/combined_diff_bg"
+
+#code_binary = "/bench/PhD/NGS_binaries/BMI/coded_obs_set"
+#matrix_output = "/bench/PhD/NGS_binaries/BMI/BGHMM_sib_matrix"
+sib_e_ui_pth = "/bench/PhD/NGS_binaries/BMI/sib_e_ui"
+sib_e_inf_pth = "/bench/PhD/NGS_binaries/BMI/sib_e_inf"
+rys_e_ui_pth = "/bench/PhD/NGS_binaries/BMI/rys_e_ui"
+rys_e_inf_pth = "/bench/PhD/NGS_binaries/BMI/rys_e_inf"
+combined_e_ui_pth = "/bench/PhD/NGS_binaries/BMI/combined_e_ui"
+combined_e_inf_pth = "/bench/PhD/NGS_binaries/BMI/combined_e_inf"
+
+
+#JOB CONSTANTS
+const ensemble_size = 1000
+const no_sources = 8
+const source_min_bases = 3
+const source_max_bases = 10
+@assert source_min_bases < source_max_bases
+const source_length_range= source_min_bases:source_max_bases
+const mixing_prior = .07
+@assert mixing_prior >= 0 && mixing_prior <= 1
+const prior_wt=1.2
+
+@info "Loading master libraries..."
+using Distributed, Serialization
+
+@info "Adding librarians and workers..."
+no_local_procs=2
+no_remote_procs=6
+remote_machine = "10.0.0.3"
+remote_pool=addprocs([(remote_machine, no_remote_procs)], tunnel=true, topology=:master_worker)
+local_pool=addprocs(no_local_procs, topology=:master_worker)
+worker_pool=vcat(remote_pool,local_pool)
+
+@info "Loading libraries everywhere..."
+@everywhere using BioMotifInference, Random
+Random.seed!(myid()*10000)
+
+@info "Loading informative source priors..."
+sib_wms = BioMotifInference.read_fa_wms_tr(sib_wms_path)
+rys_wms = BioMotifInference.read_fa_wms_tr(rys_wms_path)
+sib_mix_prior = BioMotifInference.cluster_mix_prior!(deserialize(sib_df_binary), sib_wms)
+rys_mix_prior = BioMotifInference.cluster_mix_prior!(deserialize(rys_df_binary), rys_wms)
+
+@info "Filtering informative priors..."
+sib_prior_wms = BioMotifInference.filter_priors(Int(floor(no_sources/2)), source_max_bases, sib_wms, sib_mix_prior)
+rys_prior_wms = BioMotifInference.filter_priors(Int(floor(no_sources/2)), source_max_bases, rys_wms, rys_mix_prior)
+combined_prior_wms = BioMotifInference.combine_filter_priors(Int(floor(no_sources/2)), source_max_bases, (sib_wms, rys_wms), (sib_mix_prior, rys_mix_prior))
+
+@info "Assembling informative source priors..."
+sib_inf_sp = BioMotifInference.assemble_source_priors(no_sources, sib_prior_wms, prior_wt)
+rys_inf_sp = BioMotifInference.assemble_source_priors(no_sources, rys_prior_wms, prior_wt)
+combined_inf_sp = BioMotifInference.assemble_source_priors(no_sources, combined_prior_wms, prior_wt)
+
+@info "Assembling uninformative source priors..."
+sib_ui_sp = BioMotifInference.assemble_source_priors(no_sources, Vector{Matrix{Float64}}())
+rys_ui_sp = BioMotifInference.assemble_source_priors(no_sources, Vector{Matrix{Float64}}())
+combined_ui_sp = BioMotifInference.assemble_source_priors(no_sources, Vector{Matrix{Float64}}())
+
+@info "Loading BGHMM likelihood matrix binaries..."
+sib_matrix=deserialize(sib_diff_bg)
+rys_matrix=deserialize(rys_diff_bg)
+combined_matrix=deserialize(combined_diff_bg)
+
+@info "Loading coded observation sets..."
+sib_obs = deserialize(sib_code_binary)
+rys_obs = deserialize(rys_code_binary)
+combined_obs = deserialize(combined_code_binary)
+
+@info "Assembling sib IPM ensemble on uninformative priors..."
+isfile(string(sib_e_ui_pth,'/',"ens")) ? (sib_e_ui = deserialize(string(sib_e_ui_pth,'/',"ens"))) :
+    (sib_e_ui = BioMotifInference.IPM_ensemble(worker_pool, sib_e_ui_pth, ensemble_size, sib_ui_sp, (falses(0,0), mixing_prior), sib_matrix, sib_obs, source_length_range))
+
+@info "Assembling sib IPM ensemble on informative priors..."
+isfile(string(sib_e_inf_pth,'/',"ens")) ? (sib_e_inf = deserialize(string(sib_e_inf_pth,'/',"ens"))) :
+        (sib_e_inf = BioMotifInference.IPM_ensemble(worker_pool, sib_e_inf_pth, ensemble_size, sib_inf_sp, (falses(0,0), mixing_prior), sib_matrix, sib_obs, source_length_range))
+    
+@info "Assembling rys IPM ensemble on uninformative priors..."
+isfile(string(rys_e_ui_pth,'/',"ens")) ? (rys_e_ui = deserialize(string(rys_e_ui_pth,'/',"ens"))) :
+    (rys_e_ui = BioMotifInference.IPM_ensemble(worker_pool, rys_e_ui_pth, ensemble_size, rys_ui_sp, (falses(0,0), mixing_prior), rys_matrix, rys_obs, source_length_range))
+
+@info "Assembling rys IPM ensemble on informative priors..."
+isfile(string(rys_e_inf_pth,'/',"ens")) ? (rys_e_inf = deserialize(string(rys_e_inf_pth,'/',"ens"))) :
+        (rys_e_inf = BioMotifInference.IPM_ensemble(worker_pool, rys_e_inf_pth, ensemble_size, rys_inf_sp, (falses(0,0), mixing_prior), rys_matrix, rys_obs, source_length_range))
+
+@info "Assembling combined IPM ensemble on uninformative priors..."
+isfile(string(combined_e_ui_pth,'/',"ens")) ? (combined_e_ui = deserialize(string(combined_e_ui_pth,'/',"ens"))) :
+    (combined_e_ui = BioMotifInference.IPM_ensemble(worker_pool, combined_e_ui_pth, ensemble_size, combined_ui_sp, (falses(0,0), mixing_prior), combined_matrix, combined_obs, source_length_range))
+
+@info "Assembling combined IPM ensemble on informative priors..."
+isfile(string(combined_e_inf_pth,'/',"ens")) ? (combined_e_inf = deserialize(string(combined_e_inf_pth,'/',"ens"))) :
+        (combined_e_inf = BioMotifInference.IPM_ensemble(worker_pool, combined_e_inf_pth, ensemble_size, combined_inf_sp, (falses(0,0), mixing_prior), combined_matrix, combined_obs, source_length_range))
+
+rm(worker_pool)
+
+@info "Job done!"
